c2bc1129486720939087199b80c6042d3d0f4375
JustBotsa <justbotsa@gmail.com>
2023-03-17 01:24:46 +0100

    c hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /*
-    main - print text using puts
-    Return:0 
-*/
+ *     main - Hello, puts
+ *   
+ *     Return: 0
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
