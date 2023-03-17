c1152db13509507ff85bf08b8d21ffd937900d05
JustBotsa <justbotsa@gmail.com>
2023-03-17 01:52:00 +0100

    c hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- *  * main - UNIX is basically a simple operating system
- *   *
- *    * Return: 0
+ * * main -UNIX is basically a simple operating system
+ * *
+ * * Return: 1
  **/
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-			return (1);
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
