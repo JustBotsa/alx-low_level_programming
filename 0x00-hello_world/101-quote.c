cee96fe2fbff5764b13390084e11198ad55b802b
JustBotsa <justbotsa@gmail.com>
2023-03-17 01:44:25 +0100

    c hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  * main - UNIX is basically a simple operating system
+ *   *
+ *    * Return: 0
+ **/
+int main(void)
+{
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+			return (1);
+}
